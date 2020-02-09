a22bd1b0612c42532ca344118a66dbde4160669d
CentipedeZ <dolinekvit@gmail.com>
2020-02-09 17:38:21 +0100

    Report screenshot now in RichEmbed.

--- commands/grepolis/indexer/report.js
+++ commands/grepolis/indexer/report.js
@@ -1,8 +1,9 @@
 const { get } = require('axios');
+const { RichEmbed } = require('discord.js');
 
 exports.run = async (client, message) => {
     get(`${process.env.BACKEND_URL}/discord/hash?guild=${message.guild.id}&hash=${message.content}`)
-        .then((response) => message.channel.send(response.data.url))
+        .then((response) => message.channel.send(new RichEmbed().setImage(response.data.url).setColor(0x18bc9c)))
         .catch(() => message.channel.send('Could not get image for this report hash.'));
 };
 
